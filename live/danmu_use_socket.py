d46e11d446916d469f9ed872d35ca309f4ba20af
owo931214 <owo931214@gmail.com>
2023-02-15 00:04:51 +0800

    heartbeat

--- live/danmu_use_socket.py
+++ live/danmu_use_socket.py
@@ -1,11 +1,20 @@
 import json
+import threading
 import time
 import zlib
 
 import websocket
 
 
-def on_open(wsapp):
+def heartbeating(self):
+    while True:
+        heart = bytes([0, 0, 0, 18, 0, 16, 0, 1, 0, 0, 0, 2, 0, 0, 0, 1])
+        self.send(heart)
+        print(">>----Heartbeat----")
+        time.sleep(50)
+
+
+def on_open(self):
     join_data = {
         "roomid": 24393,
         "uid": 0,
@@ -14,49 +23,50 @@ def on_open(wsapp):
     header = bytes([0, 0, 0, 16 + len(body), 0, 16, 0, 1, 0, 0, 0, 7, 0, 0, 0, 1])
     message = header + body
     print("Connection opened.")
-    ws.send(message, opcode=websocket.ABNF.OPCODE_BINARY)
+    self.send(message)
     print("Joined.")
+    threading.Thread(target=heartbeating, args=(self,)).start()
 
 
-def on_message(wsapp, message):
+def on_message(self, message):
     pt = message[7]
-    if pt == 2:
+    op = message[11]
+    if pt == 1:
+        print("<<----Protocol code: 1----")
+        if op == 3:
+            print("Get heartbeat response")
+        if op == 8:
+            print(f"Successfully entered the room | Received message: {json.loads(message[16:])}")
+    elif pt == 2:
         message = zlib.decompress(message[16:])
         base = 0
         end = len(message)
         datas = []
         while True:
-            body_len = (message[base] << 24) + (message[base + 1] << 16) + \
-                       (message[base + 2] << 8) + message[base + 3] - 16
+            body_len = (message[base] << 24) + (message[base + 1] << 16) + (message[base + 2] << 8) + message[
+                base + 3] - 16
             datas.append(json.loads(message[base + 16: base + 16 + body_len]))
             base = base + 16 + body_len
             if base == end:
                 break
+        print("<<----Protocol code: 2----")
         for data in datas:
-            print(f"Received message: {data}")
+            print(f"Opcode: {op} | Received message: {data}")
     else:
-        data = json.loads(message[16:])
-        print(f"Received message: {data}")
+        print("Could not process message")
 
 
-
-def on_ping(wsapp):
-    heart = bytes([0, 0, 0, 18, 0, 16, 0, 1, 0, 0, 0, 2, 0, 0, 0, 1, 123, 125])
-    wsapp.send(heart.decode("utf-8"))
-    print("Ping.")
-
-
-def on_error(wsapp, error):
+def on_error(self, error):
     print(f"Error: {error}")
 
 
-def on_close(wsapp, *args):
+def on_close(self, *args):
     print(f"Connection closed with: {args}")
 
 
 if __name__ == "__main__":
     time_start = time.time()
-    ws = websocket.WebSocketApp("wss://broadcastlv.chat.bilibili.com:2245/sub", on_open=on_open, on_message=on_message,
-                                on_error=on_error, on_close=on_close, on_ping=on_ping)
-    ws.run_forever(ping_interval=30, ping_payload="payload")
+    wsapp = websocket.WebSocketApp("wss://broadcastlv.chat.bilibili.com:2245/sub", on_open=on_open,
+                                   on_message=on_message, on_error=on_error, on_close=on_close)
+    wsapp.run_forever()
     print(time.time() - time_start)

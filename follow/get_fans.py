d5aa9a665d98d337b796d0d7b5c00943242ed2d8
owo931214 <91181169+owo931214@users.noreply.github.com>
2023-03-03 15:39:58 +0800

    Add files via upload

--- follow/get_fans.py
+++ follow/get_fans.py
@@ -4,18 +4,26 @@ import random
 import threading
 import json
 
-user_cookie = {"SESSDATA":"", "buvid3":"9C2E1061-55EE-B5D9-F0C8-E040FF955F4C08928infoc", "bili_jct":"f9829afb92484fb401e5f032f1c1654f"}
+user_cookie = {"SESSDATA":"894cb883%2C1693378035%2C03451%2A31", "buvid3":"D16EC6A1-C027-3A0B-2343-9EBE60ED82EE11272infoc", "bili_jct":"d1d292f68baeed574696f09d40cc0d43"}
 def check_status(targets, cookies):
+    dict = {}
     for target in targets:
         result = requests.get(url=f"https://api.bilibili.com/x/space/acc/relation?mid={str(target)}",cookies=cookies)
-        print(result.text)
-        if json.loads(result.text)["code"] == -101:
-            raise KeyboardInterrupt
-        time.sleep(random.gauss(mu=0.2,sigma=0.1))
-
+        match json.loads(result.text)["code"]:
+            case -101:
+                raise BaseException("未登入")
+            case -400:
+                raise BaseException("請求錯誤")
+            case 0:
+                dict[target] = json.loads(result.text)["data"]
+        time.sleep(max(random.gauss(mu=0.2,sigma=0.1),0.05))
+    return dict
+    
 def keep_running(target, cookie):
     while(True):
         threading.Thread(target=check_status, args=(target, cookie,))
 
 if __name__ == "__main__":
-    check_status(targets=[35798955],cookies=user_cookie)
+    result = check_status(targets=[128912828, 35798955, 4141795, 484819462], cookies=user_cookie)
+    for _ in result:
+        print(result[_], "\n")
--- live/danmu_use_socket.py
+++ live/danmu_use_socket.py
@@ -2,6 +2,7 @@ import json
 import threading
 import time
 import zlib
+import brotli
 
 import websocket
 
@@ -13,6 +14,18 @@ def heartbeating(self):
         print(">>----Heartbeat----")
         time.sleep(50)
 
+def load_msg(message):
+    base = 0
+    end = len(message)
+    datas = []
+    while True:
+        body_len = (message[base] << 24) + (message[base + 1] << 16) + (message[base + 2] << 8) + message[
+            base + 3] - 16
+        datas.append(json.loads(message[base + 16: base + 16 + body_len]))
+        base = base + 16 + body_len
+        if base == end:
+            break
+    return datas
 
 def on_open(self):
     join_data = {
@@ -31,29 +44,31 @@ def on_open(self):
 def on_message(self, message):
     pt = message[7]
     op = message[11]
-    if pt == 1:
-        print("<<----Protocol code: 1----")
-        if op == 3:
-            print("Get heartbeat response")
-        if op == 8:
-            print(f"Successfully entered the room | Received message: {json.loads(message[16:])}")
-    elif pt == 2:
-        message = zlib.decompress(message[16:])
-        base = 0
-        end = len(message)
-        datas = []
-        while True:
-            body_len = (message[base] << 24) + (message[base + 1] << 16) + (message[base + 2] << 8) + message[
-                base + 3] - 16
-            datas.append(json.loads(message[base + 16: base + 16 + body_len]))
-            base = base + 16 + body_len
-            if base == end:
-                break
-        print("<<----Protocol code: 2----")
-        for data in datas:
-            print(f"Opcode: {op} | Received message: {data}")
-    else:
-        print("Could not process message")
+    match pt:
+        case 1:
+            print("<<----Protocol code: 1----")
+            match op:
+                case 3:
+                    print("Get heartbeat response")
+                case 8:
+                    print(f"Successfully entered the room | Received message: {json.loads(message[16:])}")
+                case _:
+                    print("Could not process message{json.loads(message[16:])}")
+        case 2:
+            message = zlib.decompress(message[16:])
+            datas = load_msg(message)
+            print("<<----Protocol code: 2----")
+            for data in datas:
+                print(f"Opcode: {op} | Received message: {data}")
+        case 3:
+            message = brotli.decompress(message[16:])
+            datas = load_msg(message)
+            print("<<----Protocol code: 3----")
+            for data in datas:
+                print(f"Opcode: {op} | Received message: {data}")
+        case _:
+            print(f"<<----Protocol code: {pt}----")
+            print(f"Could not process message\n{message[16:]}")
 
 
 def on_error(self, error):
@@ -66,7 +81,7 @@ def on_close(self, *args):
 
 if __name__ == "__main__":
     time_start = time.time()
-    wsapp = websocket.WebSocketApp("wss://broadcastlv.chat.bilibili.com:2245/sub", on_open=on_open,
+    wsapp = websocket.WebSocketApp("wss://broadcastlv.chat.bilibili.com/sub", on_open=on_open,
                                    on_message=on_message, on_error=on_error, on_close=on_close)
     wsapp.run_forever()
     print(time.time() - time_start)

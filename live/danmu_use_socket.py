95f265610183c5bad4d136dd343c832bdd7b4925
owo931214 <91181169+owo931214@users.noreply.github.com>
2023-03-08 11:51:27 +0800

    Add files via upload

--- live/danmu_use_socket.py
+++ live/danmu_use_socket.py
@@ -15,18 +15,66 @@ def heartbeating(self):
         time.sleep(30)
 
 
-def load_msg(message):
-    base = 0
-    end = len(message)
-    datas = []
-    while True:
-        body_len = (message[base] << 24) + (message[base + 1] << 16) + (message[base + 2] << 8) + message[base + 3] - 16
-        datas.append(json.loads(message[base + 16: base + 16 + body_len]))
-        base = base + 16 + body_len
-        if base == end:
-            break
-    return datas
-
+def parsing_msg(message):
+    pt = message[7]
+    op = message[11]
+    print(f"<<----Protocol code: {pt}----")
+    match pt:
+        case 0 | 1:
+            match op:
+                case 3:
+                    print("[ 心跳回復 ]")
+                case 5:
+                    data = json.loads(message[16:])
+                    if data['cmd'] == 'STOP_LIVE_ROOM_LIST':
+                        print(f"[ 下播的直播間 ] pass")
+                        
+                    else:
+                        print(f"[ 廣播通知 ] {message[16:]}")
+                case 8:
+                    print(f"[ 成功進入直播間 ]")
+                case _:
+                    print(f"[ 未知 ] Opcode: {op} | Received message: {message[16:]}")
+        case 2 | 3:
+            if pt == 2:
+                message = zlib.decompress(message[16:])
+            elif pt == 3:
+                message = brotli.decompress(message[16:])
+             
+            base = 0
+            end = len(message)
+            datas = []
+            while True:
+                body_len = (message[base] << 24) + (message[base + 1] << 16) + (message[base + 2] << 8) + message[base + 3] - 16
+                datas.append(json.loads(message[base + 16: base + 16 + body_len]))
+                base = base + 16 + body_len
+                if base == end:
+                    break
+            # TODO
+            for data in datas:
+                match data['cmd']:
+                    case 'COMBO_SEND':
+                        pass # TODO
+                    case 'DANMU_MSG':
+                        danmu_msg = {"msg":data['info'][1], "time":data['info'][0][5], "send_by":{"uid":data['info'][2][0], "name":data['info'][2][1], "is_admin":bool(data['info'][2][2]), "is_vip":bool(data['info'][2][3])}}
+                        print(f"[ 彈幕訊息 ] {danmu_msg}\n{data}")
+                    case 'ENTRY_EFFECT':
+                        effect = {data['data']}
+                        print(f"[ 特效 ] {effect}")
+                    case 'GUARD_BUY':
+                        guard_msg = {"level":{data['data']['gift_name']}} # TODO
+                    case 'HOT_RANK_CHANGED':
+                        pass # TODO
+                    case 'INTERACT_WORD':
+                        interact_msg = {"act":"關注"if data['data']['msg_type'] == 2 else "進場", "time":data['data']['timestamp'], "trigger_by":{"uid":data['data']['uid'], "name":data['data']['uname']}}
+                        print(f"[ 交互訊息 ] {interact_msg}")
+                    case 'ONLINE_RANK_COUNT':
+                        print(f"[ 高能用戶數量 ] {data['data']['count']}人")
+                    case 'WATCHED_CHANGE':
+                        print(f"[ 觀看人數 ] {data['data']['num']}人看過")
+                    case _:
+                        print(f"[ 未知 ] {data}")
+        
 
 def on_open(self):
     join_data = {
@@ -36,35 +84,12 @@ def on_open(self):
     body = json.dumps(join_data).encode('utf-8')
     header = bytes([0, 0, 0, 16 + len(body), 0, 16, 0, 1, 0, 0, 0, 7, 0, 0, 0, 1])
     message = header + body
-    print("Connection opened.")
     self.send(message)
-    print("Joined.")
     threading.Thread(target=heartbeating, args=(self,)).start()
 
 
 def on_message(self, message):
-    pt = message[7]
-    op = message[11]
-    print(f"<<----Protocol code: {pt}----")
-    match pt:
-        case 0, 1:
-            match op:
-                case 3:
-                    print("Get heartbeat response")
-                case 8:
-                    print(f"Successfully entered the room | Received message: {json.loads(message[16:])}")
-                case _:
-                    print(f"Could not process message: {json.loads(message[16:])}")
-        case 2:
-            message = zlib.decompress(message[16:])
-            datas = load_msg(message)
-            for data in datas:
-                print(f"Opcode: {op} | Received message: {data}")
-        case 3:
-            message = brotli.decompress(message[16:])
-            datas = load_msg(message)
-            for data in datas:
-                print(f"Opcode: {op} | Received message: {data}")
+    parsing_msg(message)
 
 
 def on_error(self, error):

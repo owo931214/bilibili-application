0b382fedfa3d6c1d643e17bb5e680276b353ba3e
owo931214 <91181169+owo931214@users.noreply.github.com>
2023-04-21 11:51:27 +0800

    Add files via upload

---
+++ bilibili-application-master/run.py
@@ -0,0 +1,14 @@
+# time_start = time.time()
+# wsapp = LiveSocket(room_id=768756)  # 128912828 沙月, 22320946 純寶, 4141795 小沙月, 768756 滋蹦, 24393 雪狐
+# print(time.time() - time_start)
+import sys
+
+from PyQt5.QtWidgets import QApplication
+
+from live.ui_ctrl import Danmuji
+from live.socket import LiveSocket
+
+app = QApplication(sys.argv)
+ui = Danmuji()
+ui.msg_test()
+sys.exit(app.exec_())
